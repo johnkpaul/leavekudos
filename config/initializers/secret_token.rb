4f19556d9cb8d507af0b36df1cc095bbd2e70f34
Ben Burton <bburton@bburton-macbook.local>
2013-01-05 09:35:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leavekudos::Application.config.secret_token = '8323ffdec9a81e21757f140870b37aef26a3d2e8fe3077d5bdefd056c311aae7bb29219ee25664a515f858d8aebcbd01c1e45896ca728b6ce56769e09a766551'
