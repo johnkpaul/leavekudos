3bf93a7ae285b7c9245fcd9449c51a64e6b9365e
Ryan J <ryan@roflopoly.com>
2013-01-05 17:40:12 -0500

    Parameterize redirect_url

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -13,12 +13,12 @@ class OauthController < ApplicationController
 
   def register
     # Start the oauth flow
-    redirect_to oauth2_client.auth_code.authorize_url(:redirect_uri => 'http://localhost:3000/callback')
+    redirect_to oauth2_client.auth_code.authorize_url(:redirect_uri => Settings.foursq_api.redirect_url)
   end
 
   def callback
     access_token = oauth2_client.auth_code.get_token(params[:code], 
-                                                    :redirect_uri => 'http://localhost:3000/callback')
+                                                    :redirect_uri => Settings.foursq_api.redirect_url)
     cookies[:fsq_token] = access_token.token
     client = FoursqWrapper.create_authenticated_client(access_token.token)
     foursq_user = client.user('self')
