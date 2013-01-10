f7291d81872b52388ad2bad89f53232b6e94b49d
Ryan J <ryan@roflopoly.com>
2013-01-10 15:27:56 -0500

    Remove devise from user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise 
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :foursquare_user_id, :foursquare_user_hash
