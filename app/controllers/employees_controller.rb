f53c4667365411c0b16dfb52b86ffdfc32c5324e
John Paul <john@johnkpaul.com>
2013-01-12 16:37:14 -0500

    add CORS support for other endpoints

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,14 @@
 class EmployeesController < ApplicationController
 
+  after_filter :set_access_control_headers
+
   def show
     render json: Employee.where(venue_id: params[:venue_id])
+  end
+
+  def set_access_control_headers 
+      headers['Access-Control-Allow-Origin'] = '*' 
+      headers['Access-Control-Request-Method'] = '*' 
   end
 
 end
--- app/controllers/foursq_passthrough_controller.rb
+++ app/controllers/foursq_passthrough_controller.rb
@@ -2,9 +2,16 @@ require 'foursquare_wrapper'
 
 class FoursqPassthroughController < ApplicationController
 
+  after_filter :set_access_control_headers
+
   def checkins
     logger.debug "Fetching most recent checkins"
     render json: FoursquareWrapper.user_checkins(cookies[:fsq_token]).items
+  end
+
+  def set_access_control_headers 
+      headers['Access-Control-Allow-Origin'] = '*' 
+      headers['Access-Control-Request-Method'] = '*' 
   end
 
 end
