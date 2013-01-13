9fbd2f6e05e5454d8d6514eac57c025ba9d96cbe
John Paul <john@johnkpaul.com>
2013-01-13 14:19:26 -0500

    fix paths for splash page

--- public/www/app/scripts/pre-login-main.js
+++ public/www/app/scripts/pre-login-main.js
@@ -1,7 +1,7 @@
 // Require.js allows us to configure shortcut alias
 // Their usage will become more apparent futher along in the tutorial.
 require.config({
-  baseUrl: 'www/js', 
+  baseUrl: 'www/app/scripts', 
   paths: {
     jquery: 'libs/jquery/jquery-min',
     underscore: 'libs/underscore/underscore-min',
@@ -20,9 +20,23 @@ require.config({
 });
 
 require([
+  'app',
   'collections/kudos/KudosCollection',
   'views/KudosView'
-], function(KudosCollection, KudosView){
+], function(App, KudosCollection, KudosView){
+      var isDev = window.location.href.indexOf('localhost') > -1;
+      var options = {
+        oauthToken:'',
+        username:'', 
+        mode:isDev ? "DEV" : "PROD",
+        baseUrl:isDev ? "http://localhost:3000" : "http://www.leavekudos.com"
+      };
+
+      if(document.cookie.indexOf('fsq_token') > -1) { 
+        App.start(options);
+      } else {
+        window.location.assign('/connect');
+      }
       var kudosCollection = new KudosCollection();
       kudosCollection.fetch().then(function(){
         var kudosView = new KudosView({collection:kudosCollection});
