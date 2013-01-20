6fcd2d5a24e83507e631e1e519e5531659064887
Ryan J <ryan@roflopoly.com>
2013-01-20 16:15:30 -0500

    Use production user.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,11 @@
+# It's all the same machine
 server 'leavekudos.com', :app, :web, :primary => true
 
-set :deploy_to, "/opt/webapps/leavekudos.com"
+set :user, "leavekudos" # The server's user for deploys
 
-role :web, "leavekudos.com"                          # Your HTTP server, Apache/etc
-role :app, "leavekudos.com"                          # This may be the same as your `Web` server
+set :deploy_to, "/opt/webapps/www.leavekudos.com"
+
+role :web, "leavekudos.com" # Your HTTP server, Apache/etc
+role :app, "leavekudos.com" # This may be the same as your `Web` server
 role :db,  "leavekudos.com", :primary => true # This is where Rails migrations will run
 
